0e9228b207c9ea7dfb564cd1608f1795a7ccdd6b
Nirali <nirali.bhanderi@tatvasoft.com>
2022-12-13 15:40:13 +0530

    fix error handling

--- src/controller/plaid.ts
+++ src/controller/plaid.ts
@@ -19,25 +19,21 @@ export const getTransactions: RequestHandler = async (
   req: Request,
   res: Response
 ) => {
-  const { publicToken } = req.body;
-  const accessToken = await plaid
-    .exchangeAccessToken(publicToken)
-    .catch((err) => {
-      console.log('failed to get access token', err);
-      throw new Error(err);
-    });
-  const transactionData = await plaid
-    .getTransactions(accessToken, req.query)
-    .catch((err) => {
-      console.log('failed to get transacrions', err);
-      throw new Error(err);
-    });
-  const {
-    added: data,
-    has_more: hasMore,
-    next_cursor: cursor
-  } = transactionData;
-  return res
-    .status(200)
-    .json({ message: 'Transactions successfully', data, hasMore, cursor });
+  const { publictoken } = req.headers;
+  try {
+    const accessToken = await plaid.exchangeAccessToken(publictoken as string);
+    const transactionData = await plaid.getTransactions(accessToken, req.query);
+    const {
+      added: data,
+      has_more: hasMore,
+      next_cursor: cursor
+    } = transactionData;
+    return res
+      .status(200)
+      .json({ message: 'Transactions successfully', data, hasMore, cursor });
+  } catch (error: any) {
+    return res
+      .status(error.response.status || 500)
+      .json({ message: error.response.statusText });
+  }
 };
